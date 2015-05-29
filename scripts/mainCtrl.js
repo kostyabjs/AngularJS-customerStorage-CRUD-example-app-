22e1b233d25452829d019548fc727e0bc9a1c67a
kostyamuzychenko <kostya.muzychenko@gmail.com>
2015-05-29 11:16:04 +0300

    fix commit

--- scripts/mainCtrl.js
+++ scripts/mainCtrl.js
@@ -2,9 +2,17 @@
 
 angular.module('angularTest', ['ui.bootstrap'])
     .controller('MainCtrl', ['$scope', '$modal', function ($scope, $modal) {
-        $scope.customers = [{name: "Joe", email: "joe@gmail.com", telephone: "123-345", address: "undefined", street: "Some street", city: "Some city", state: "Some state", zip: "65"},
-            {name: "John", email: "john@gmail.com", telephone: "234-567", address: "undefined", street: "Some street#2", city: "Some city#2", state: "Some state", zip: "66"}];
+        //Data storage implemented via JSON
+        $scope.customers = [{
+                name: "Joe", email: "joe@gmail.com", telephone: "123-345", address: "undefined",
+                street: "Some street", city: "Some city", state: "Some state", zip: "65"
+            },
+            {
+                name: "John", email: "john@gmail.com", telephone: "234-567", address: "undefined",
+                street: "Some street#2", city: "Some city#2", state: "Some state", zip: "66"
+            }];
 
+        //List of states to be chosen in modal
         $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
             'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia',
             'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
@@ -14,6 +22,7 @@ angular.module('angularTest', ['ui.bootstrap'])
             'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
             'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
+        //Modal window
         $scope.open = function (customer) {
             $modal.open({
                 templateUrl: '../views/modal/addCustomerModal.html',
@@ -26,6 +35,7 @@ angular.module('angularTest', ['ui.bootstrap'])
                         $scope.interim = editCustomer;
                     }
 
+                    //Function called on press to OK button
                     $scope.ok = function () {
                         if (!editCustomer) {
                             $scope.customers.push($scope.interim);
@@ -40,6 +50,8 @@ angular.module('angularTest', ['ui.bootstrap'])
                         }
                         $modalInstance.close();
                     };
+
+                    //Function called on press to Cancel button
                     $scope.cancel = function () {
                         $modalInstance.dismiss('cancel');
                     };
@@ -52,9 +64,11 @@ angular.module('angularTest', ['ui.bootstrap'])
             });
         };
 
-
+        //Function removes customer from storage
         $scope.removeRow = function (name) {
-            for (var i = 0; i < $scope.customers.length; i++) {
+            var i = 0;
+
+            for (i; i < $scope.customers.length; i++) {
                 if ($scope.customers[i].name === name) {
                     $scope.customers.splice(i, 1);
                     break;
